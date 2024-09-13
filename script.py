f3a51ebb8a0e9dd9ecf53a20f578592bc89a5459
Matthew <matthew.nguyen.ca2023@gmail.com>
2024-09-12 21:40:53 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+#Practicing with variables strings and values
+
 lovely_loveseat_description =( "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white." )
 lovely_loveseat_price = 254.00
 
